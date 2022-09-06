077879fc59caa0a4e2902883575e790e43ff2780
Tay Smith <taysmith321@gmail.com>
2022-09-06 12:45:09 -0400

    Routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
+import { PathLocationStrategy } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +15,7 @@ export class NavComponent implements OnInit {
   model: any = {}
   currentUser$: Observable<User>;
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
 
@@ -20,14 +23,15 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members')
     }, error => {
       console.log(error)
+      this.toastr.error(error.error)
     });
   }
 
   logout() {
     this.accountService.logout();
-
+    this.router.navigateByUrl('/')
   }
 }
